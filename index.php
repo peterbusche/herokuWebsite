e71f500ffc584b36690c17a9db5f3d32bde14cf5
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-11 17:58:55 -0600

    15

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php require_once("nav.php"); ?>
+
 <div id="content">
  PetersPlayhouse
+</div>
+
+<div class="container">
+    <div class="col"<p>hello</p></div>
+    <div class="col"<p>world</p></div>
+    <div class="col"<p>poop</p></div>
 </div>
 
 
--- nav.php
+++ nav.php
@@ -5,10 +5,10 @@
     <link rel="stylesheet" href="styles/scales.css">
     <link rel="stylesheet" href="styles/buttons.css">
     <link rel="stylesheet" href="styles/text.css">
+    <link rel="stylesheet" href="styles/boxes.css">
 
   </head>
   <body>
-     <img src="https://static.wikia.nocookie.net/rimworld-bestiary/images/f/f3/Dinosauria_velociraptor_f_east.png"/>
      <h1>Peter's Playhouse</h1>
      
      
@@ -16,25 +16,17 @@
          <ul>
 
             <li><a href="index.php"><button class="join-button">HOME</button></a></li>
-            <li><a href="AllGames.php"><button class="join-button">All Games</button></a></li>
-            <li><a href="Multiplayer.php"><button class="join-button">Multiplayer</button></li>
-            <li><a href="SinglePlayer.php">Single Player</a></li>
-            <li><a href="Random.php">Random</a></li>
-
-
-
-
-
-            <!--
-           <li><a href="/dinos/index.php">Home</a></li>
-           <li><a href="/dinos/comments.php">Comments</a></li>
-           <li><a href="/dinos/favorites.php">Favorites</a></li>
-           <li><a href="/dinos/about.php">About</a></li>
-           <li><a href="/dinos/careers.php">Careers</a></li>
-           <li><a href="/dinos/logout.php">Logout</a></li>
--->
-           
+            <li><a href="AllGames.php"><button class="join-button">ALL GAMES</button></a></li>
+            <li><a href="Multiplayer.php"><button class="join-button">MULTIPLAYER</button></li>
+            <li><a href="SinglePlayer.php"><button class="join-button">SINGLE PLAYER</button></li>
+            <li><a href="Random.php"><button class="join-button">RANDOM</button></a></li>     
          </ul>
       </div>
+
+
+
+
+
+
 
       
