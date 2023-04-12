e4699e95f47a964286875e4423580fa87984cd2d
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-12 15:38:38 -0600

    52

--- nav.php
+++ nav.php
@@ -17,8 +17,8 @@
 
             <li><a href="index.php"><button class="join-button">HOME</button></a></li>
             <li><a href="AllGames.php"><button class="join-button">ALL GAMES</button></a></li>
-            <li><a href="Multiplayer.php"><button class="join-button">MULTIPLAYER</button></li>
-            <li><a href="SinglePlayer.php"><button class="join-button">SINGLE PLAYER</button></li>
+            <li><a href="Multiplayer.php"><button class="join-button">MULTIPLAYER</button></a></li>
+            <li><a href="SinglePlayer.php"><button class="join-button">SINGLE PLAYER</button></a></li>
             <li><a href="Random.php"><button class="join-button">RANDOM</button></a></li>     
          </ul>
       </div>
