21ad6d8f46dbc9000fd428586aed4d80da1e8c1f
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-11 18:33:13 -0600

    21

--- index.php
+++ index.php
@@ -8,12 +8,19 @@
 -->
 
 
+<!--
 <div class="container">
     <div class="col"<p>hello</p></div>
     <div class="col"<p>world</p></div>
     <div class="col"<p>poop</p></div>
     <div class="col"<p>poopie</p></div>
 </div>
+-->
 
+<div class="first-box">
+    <p class="box-title">
+        poop
+    </p>
+</div>
 
 <?php include("footer.php"); ?>
