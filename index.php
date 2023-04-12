fabdf6e29e7de7c20a087ae4c368e4753370da1d
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-12 13:20:07 -0600

    36

--- index.php
+++ index.php
@@ -39,24 +39,27 @@
 <div class="big_box w3-main w3-content w3-padding">
   <div class = "inner_box">
     <div class="quarter">
-      alskdjfk
+    <img src="/thumbnails/steak.jpg" alt="Steak" style="width:100%">
       <h3>aslkdfjl</h3>
-      <p>asldfkjalskdfjalskdfjkasdf</p>
+      
+      
     </div>
     <div class="quarter">
-      alskdjfk
+    <img src="/thumbnails/steak.jpg" alt="Steak" style="width:100%">
       <h3>aslkdfjl</h3>
-      <p>asldfkjalskdfjalskdfjkasdf</p>
+      
+      
     </div>
     <div class="quarter">
-      alskdjfk
+    <img src="/thumbnails/steak.jpg" alt="Steak" style="width:100%">
       <h3>aslkdfjl</h3>
-      <p>asldfkjalskdfjalskdfjkasdf</p>
+      
+      
     </div>
     <div class="quarter">
-      alskdjfk
+    <img src="/thumbnails/steak.jpg" alt="Steak" style="width:100%">
       <h3>aslkdfjl</h3>
-      <p>asldfkjalskdfjalskdfjkasdf</p>
+      
     </div>
   </div>
 </div>
