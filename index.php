3fa2761ea046e9faa5b8d1258234dfe64f60bcc6
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-11 18:22:00 -0600

    20

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php require_once("nav.php"); ?>
 
 
-
+<!--
 <div id="content">
     PetersPlayhouse
-    <div class="container">
-        <div class="col"<p>hello</p></div>
-        <div class="col"<p>world</p></div>
-        <div class="col"<p>poop</p></div>
-        <div class="col"<p>poopie</p></div>
-    </div>
 </div>
+-->
 
+
+<div class="container">
+    <div class="col"<p>hello</p></div>
+    <div class="col"<p>world</p></div>
+    <div class="col"<p>poop</p></div>
+    <div class="col"<p>poopie</p></div>
+</div>
 
 
 <?php include("footer.php"); ?>
