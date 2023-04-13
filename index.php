382993ef2184bd230c88a20437a13d60f5659157
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-13 14:35:14 -0600

    68

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <div class="blankGPT"></div>
 		<div class="columnGPT">
 			<div class="boxGPT">
-        <a href="game1.php"><button class="img"><img src="/thumbnails/download.png" alt="Steak"></button></a>
+        <a href="game1.php"><button class="img-button"><img src="/thumbnails/download.png" alt="Steak"></button></a>
         <h3>1</h3>
       </div>
 			<div class="boxGPT">
