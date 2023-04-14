5a9ade9ea9974957138db3a20eaf7810d8d16d12
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-13 18:31:01 -0600

    82

--- about.php
+++ about.php
@@ -2,4 +2,22 @@
 <div id="title">
    <h2>About Us</h2> 
 </div>
+<div class="containerGPT">
+   <div class="blankGPT"></div>
+      <p>asdhflkajsdhflkajsdfhlkajsdfhkajsdhflaksjdfhlaksjdfh
+         alksjdfhalksdjfhaslkdjfhasldkfjahsldkfjaskldfhaksdlfk
+         aksjdfhlasdkjfhalsdkfjhasdlfkjahsdflkajsdfhlaskdjfha
+         aksjdfhalksdjfhalskdjfhalsdkjfhalskdjfhalskdjfhlasjd
+      </p>
+      <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+      <p>asdhflkajsdhflkajsdfhlkajsdfhkajsdhflaksjdfhlaksjdfh
+         alksjdfhalksdjfhaslkdjfhasldkfjahsldkfjaskldfhaksdlfk
+         aksjdfhlasdkjfhalsdkfjhasdlfkjahsdflkajsdfhlaskdjfha
+         aksjdfhalksdjfhalskdjfhalsdkjfhalskdjfhalskdjfhlasjd
+      </p>
+      <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+
+   <div class="blankGPT"></div>
+</div>
+
 <?php include("footer.php"); ?>
