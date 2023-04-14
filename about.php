06d46fc403891749b1758af993955ef176413495
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-13 18:34:59 -0600

    84

--- about.php
+++ about.php
@@ -2,20 +2,20 @@
 <div id="title">
    <h2>About Us</h2> 
 </div>
-<div class="containerGPT">
+<div class="big-container">
    <div class="blankGPT"></div>
-      <p>asdhflkajsdhflkajsdfhlkajsdfhkajsdhflaksjdfhlaksjdfh
-         alksjdfhalksdjfhaslkdjfhasldkfjahsldkfjaskldfhaksdlfk
-         aksjdfhlasdkjfhalsdkfjhasdlfkjahsdflkajsdfhlaskdjfha
-         aksjdfhalksdjfhalskdjfhalsdkjfhalskdjfhalskdjfhlasjd
-      </p>
-      <hr style="height:2px;border-width:0;color:gray;background-color:gray">
-      <p>asdhflkajsdhflkajsdfhlkajsdfhkajsdhflaksjdfhlaksjdfh
-         alksjdfhalksdjfhaslkdjfhasldkfjahsldkfjaskldfhaksdlfk
-         aksjdfhlasdkjfhalsdkfjhasdlfkjahsdflkajsdfhlaskdjfha
-         aksjdfhalksdjfhalskdjfhalsdkjfhalskdjfhalskdjfhlasjd
-      </p>
-      <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+   <p>asdhflkajsdhflkajsdfhlkajsdfhkajsdhflaksjdfhlaksjdfh
+      alksjdfhalksdjfhaslkdjfhasldkfjahsldkfjaskldfhaksdlfk
+      aksjdfhlasdkjfhalsdkfjhasdlfkjahsdflkajsdfhlaskdjfha
+      aksjdfhalksdjfhalskdjfhalsdkjfhalskdjfhalskdjfhlasjd
+   </p>
+   <hr style="height:2px;border-width:0;color:gray;background-color:gray">
+   <p>asdhflkajsdhflkajsdfhlkajsdfhkajsdhflaksjdfhlaksjdfh
+      alksjdfhalksdjfhaslkdjfhasldkfjahsldkfjaskldfhaksdlfk
+      aksjdfhlasdkjfhalsdkfjhasdlfkjahsdflkajsdfhlaskdjfha
+      aksjdfhalksdjfhalskdjfhalsdkjfhalskdjfhalskdjfhlasjd
+   </p>
+   <hr style="height:2px;border-width:0;color:gray;background-color:gray">
 
    <div class="blankGPT"></div>
 </div>
