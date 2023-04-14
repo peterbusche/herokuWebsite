ff4064161ce14805361fb06fc99d2890148bd0c9
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-13 18:10:15 -0600

    77

--- index.php
+++ index.php
@@ -3,32 +3,45 @@
 
 <div class="containerGPT">
     <div class="blankGPT"></div>
-		<div class="columnGPT">
-      <h3>Top 10 Games</h3>
+      <div class="columnGPT">
+        <h3>Top 10 Games</h3>
 
-			<div class="boxGPT">
-        <a href="game1.php"><button class="img-button"><img src="/thumbnails/download.png" alt="Steak"></button></a>
-        <h3>1</h3>
+        <div class="boxGPT">
+          <a href="game1.php"><button class="img-button"><img src="/thumbnails/download.png" alt="Steak"></button></a>
+          <h3>1</h3>
+        </div>
+        <div class="boxGPT">
+        <a href="game2.php"><button class="img-button"><img src="/thumbnails/download.png" alt="Steak"></button></a>
+          <h3>2</h3>
+        </div>
       </div>
-			<div class="boxGPT">
-      <a href="game2.php"><button class="img-button"><img src="/thumbnails/download.png" alt="Steak"></button></a>
-        <h3>2</h3>
-      </div>
-		</div>
 
 
-		<div class="columnGPT">
-      <h3>New Games</h3>
-			<div class="boxGPT">
-      <a href="game3.php"><button class="img-button"><img src="/thumbnails/download.png" alt="Steak"></button></a>
-        <h3>3</h3>
+      <div class="columnGPT">
+        <h3>New Games</h3>
+        <div class="boxGPT">
+        <a href="game3.php"><button class="img-button"><img src="/thumbnails/download.png" alt="Steak"></button></a>
+          <h3>3</h3>
+        </div>
+        <div class="boxGPT">
+        <a href="game4.php"><button class="img-button"><img src="/thumbnails/download.png" alt="Steak"></button></a>
+          <h3>4</h3>
+        </div>
       </div>
-			<div class="boxGPT">
-      <a href="game4.php"><button class="img-button"><img src="/thumbnails/download.png" alt="Steak"></button></a>
-        <h3>4</h3>
+
+      <div class="columnGPT">
+        <h3>Leaderboard</h3>
+
+        <div class="boxGPT">
+          <a href="game1.php"><button class="img-button"><img src="/thumbnails/download.png" alt="Steak"></button></a>
+          <h3>5</h3>
+        </div>
+        <div class="boxGPT">
+        <a href="game2.php"><button class="img-button"><img src="/thumbnails/download.png" alt="Steak"></button></a>
+          <h3>6</h3>
+        </div>
       </div>
-			
-		</div>
+
     <div class="blankGPT"></div>
 </div>
 
