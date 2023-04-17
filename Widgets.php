7ff2560a8d4c7fa28fb5aff8012ca922059567d1
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-17 16:47:45 -0600

    119

--- Widgets.php
+++ Widgets.php
@@ -10,15 +10,21 @@ class Widgets {
       foreach ($columnNames as $name) {
         $html .= "<th>{$name}</th>";
       }
-      
+
       $html .= "</tr></thead><tbody>";
+
+      //interate through rows, by individual row
       foreach ($rows as $row) {
         $html .= "<tr>";
-        foreach($row as $columnName => $item) {
-          if ($columnName == "image_path") { 
-            $html .= "<td><img src='{$item}'/></td>";
-          } else {
-          $html .= "<td>". htmlspecialchars($item) . "</td>";
+        foreach($row as $columnName) {
+          if(!empty($columnName['image_path'])) {
+            $html .= "<td><img src="<?= $comment['image_path'] ?>" alt="comment image" width="100"></td>";
+          }
+          // if ($columnName == "image_path") { 
+          //   $html .= "<td><img src='{$item}'/></td>";
+          // } 
+          else {
+          $html .= "<td>". htmlspecialchars($columnName) . "</td>";
           }
         }
         $html .= "<td><a href='delete_handler.php?id={$row['id']}'>X</a></td>" ;
