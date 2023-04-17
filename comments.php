93f84017dc34a27a2d593b59879adab0d5f95f9f
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-17 14:15:14 -0600

    114

--- comments.php
+++ comments.php
@@ -17,6 +17,16 @@ require_once 'Dao.php';
 
 
    Leave a Comment
+   <form id="comment_form" method="POST">
+     <br/>
+     <!--<div>Upload an image: <input type="file" id="myfile" name="myfile" /></div>
+     <br/>-->
+     <input type="text" id="comment" value="<?php echo isset($_SESSION['inputs']['comment']) ? $_SESSION['inputs']['comment'] : '' ?>" name="comment">
+     <input type="submit" value="Submit">
+   </form>
+
+
+
    <!-- <form action="process_comment.php" method="POST">
       <label for="comment">Enter your comment:</label>
       <textarea name="comment" id="comment" rows="1" cols="50"></textarea>
