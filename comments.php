d0d2d493c994cbf25ff0a2460a97ad6c6c835570
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-17 17:20:27 -0600

    125

--- comments.php
+++ comments.php
@@ -23,7 +23,7 @@ require_once 'Dao.php';
       <textarea name="comment" id="comment" rows="1" cols="50"></textarea>
       <br>
       <label for="image_path">Upload an Image:</label>
-      <input type="file" id="myfile" name="myfile">
+      <input type="text" name="image_path" id="image_path">
       <br>
       <input type="submit" value="Submit">
    </form>
