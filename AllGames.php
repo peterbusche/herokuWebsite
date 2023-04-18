e136e3215f1d990c8ba79b0b49628d0661c78cf3
PETER BUSCHE <peter.busche95@gmail.com>
2023-04-17 22:08:28 -0600

    143

--- AllGames.php
+++ AllGames.php
@@ -2,4 +2,18 @@
 <div id="content2">
    <h5>All Games</h5> 
 </div>
+
+<div class="containerGPT">
+   <div class="blankGPT"></div>
+      <div class="columnGPT">
+         <div class="boxGPT">
+         <a href="game1.php"><button class="img-button"><img src="/img/download.png" alt="Steak"></button></a>
+         </div>
+      </div>
+   <div class="blankGPT"></div>  
+</div> 
+
+
 <?php include("footer.php"); ?>
+
+
